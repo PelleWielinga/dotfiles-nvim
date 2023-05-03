edaa219de6a6a2594ae005e7376745b97fd920f9
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-05-03 23:06:31 +0200

    Add keymappings for moving around and quitting

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -6,3 +6,10 @@ user.bind_key('n', '<A-h>', ":bprevious<CR>")
 -- Keep selection when changing indent
 user.bind_key('v', '<', '<gv')
 user.bind_key('v', '>', '>gv')
+
+user.bind_key('n', '<C-h>', '<C-w>h')
+user.bind_key('n', '<C-l>', '<C-w>l')
+user.bind_key('n', '<C-j>', '<C-w>j')
+user.bind_key('n', '<C-k>', '<C-w>k')
+
+user.bind_key('n', '<C-x>', ':wa<cr>:qa<cr>')
