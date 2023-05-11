43d7e54dcfba737108b265f9a401fe09e45381d9
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-05-11 13:12:10 +0200

    Update keybindings

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -24,13 +24,19 @@ wk.register({
   d = {
     name = "Diagnostics",
     o = { vim.diagnostic.open_float, "View diagnostic" },
+    h = { vim.lsp.buf.hover, "Hover?" },
   },
 
   r = {
     name = "Refactor",
     f = { vim.lsp.buf.format, "Format code" },
     r = { vim.lsp.buf.rename, "Rename variable" },
-  }
+  },
+
+  x = {
+    name = "Execute project command"
+    -- The project commands are defined in other places
+  },
 
 }, { prefix = "," })
 
