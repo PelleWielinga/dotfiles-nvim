f30c70bf3ce78277f9a984f555145009158c8de5
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-07-03 18:29:46 +0200

    Various navigation improvements

--- lua/plugins/keys.lua
+++ lua/plugins/keys.lua
@@ -7,6 +7,21 @@ return {
       ["<A-l>"] = { ":bnext<cr>", "Next buffer" },
       ["<A-h>"] = { ":bprevious<cr>", "Previous buffer" },
       ["<C-x>"] = { ":wa<cr>:qa<cr>", "Save and close all" },
+
+      -- Redefined down/up half a screen to be a bit slower. Easier to track across the screen.
+      ["<C-d>"] = { "10j", "Move down fast"},
+      ["<C-u>"] = { "10k", "Move up fast"},
+
+      ["<leader>"] = {
+        b = {
+          name = "Buffer actions",
+          d = { [[<cmd>bn<cr><cmd>bd#<cr>]], "Close buffer without closing window" }
+        },
+        r = {
+          -- This should be configured using LSP, but the current formatting is a bit crappy
+          j = { "<cmd>%!jq .<cr>", "Format json" }
+        }
+      }
     })
 
     wk.register({
