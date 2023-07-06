48b0d8b4fd674e94a0509b2d0e172446e64d2975
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-07-06 21:04:20 +0200

    Add tmux

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,33 +1,40 @@
-local function find_files()
-  require("telescope.builtin").find_files({
-    find_command = { "rg", "--files", "--hidden", "-g", "!.git" }
-  })
-end
-
-local function find_all_files()
-  require("telescope.builtin").find_files({
-    find_command = { "rg", "--files", "--hidden", "-g", "!.git", "--no-ignore" }
-  })
-end
-
 return {
   {
     "nvim-telescope/telescope.nvim",
-    lazy = true,
     keys = {
-      { "<leader>fa", find_all_files,                                           desc = "Find files including gitignore" },
-      { "<leader>ff", find_files,                                               desc = "Find files" },
-      { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Grep" },
-      { "<leader>fs", function() require("telescope.builtin").git_status() end, desc = "Changed files" },
-    },
-    dependencies = {
-      "folke/which-key.nvim",
+      {
+        "<leader>fa",
+        function()
+          require("telescope.builtin").find_files({
+            find_command = { "rg", "--files", "--hidden", "-g", "!.git", "--no-ignore" }
+          })
+        end,
+        desc = "Find files including gitignore"
+      },
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files({
+            find_command = { "rg", "--files", "--hidden", "-g", "!.git" }
+          })
+        end,
+        desc = "Find files"
+      },
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep() end,
+        desc = "Grep"
+      },
+      {
+        "<leader>fs",
+        function() require("telescope.builtin").git_status() end,
+        desc = "Changed files"
+      },
     },
   },
 
   {
     "nvim-telescope/telescope-media-files.nvim",
-    lazy = true,
     keys = {
       { "<leader>fm", "<cmd>Telescope media_files<cr>", desc = "Find media files", }
     },
@@ -50,13 +57,22 @@ return {
   },
 
   {
+    "camgraff/telescope-tmux.nvim",
+    keys = {
+      { "<leader>ft", "<cmd>Telescope tmux sessions<cr>", "Telescope sessions" },
+    },
+    config = function()
+      require('telescope').load_extension("tmux")
+    end
+  },
+
+  {
     "cljoly/telescope-repo.nvim",
-    lazy = true,
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
     keys = {
-      { "<leader>fp", "cmd>Telescope repo list", desc = "Find projects" }
+      { "<leader>fp", "<cmd>Telescope repo list<cr>", desc = "Find projects" }
     },
     config = function()
       local telescope = require('telescope')
@@ -78,7 +94,6 @@ return {
 
   {
     "nvim-telescope/telescope-ui-select.nvim",
-    lazy = true,
     -- TODO: Can this be triggered on sime kind of event instead of at VeryLazy?
     event = "VeryLazy",
     dependencies = {
