fdf0606b8f7efe7e96769ca2dad2d3b21cd06233
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-08-02 00:26:37 +0200

    Add keybinding to dismiss notifications

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -16,6 +16,9 @@ return {
     lazy = false,
     priority = 1000,
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>nd", function() require("notify").dismiss() end, desc = "Dismiss notifications" },
+    },
     config = function()
       vim.notify = require("notify")
     end
