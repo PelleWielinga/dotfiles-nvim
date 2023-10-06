b69581a77243f71eed13a5da3e52f5514e7f8411
Pelle Wielinga <pelle.wielinga@gmail.com>
2023-10-06 22:15:17 +0200

    Add S-Tab as reverse Tab when using code completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -44,7 +44,6 @@ return {
         ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-e>"] = cmp.mapping {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
@@ -53,6 +52,13 @@ return {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s", }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
           else
             fallback()
           end
