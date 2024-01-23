1ca23ca4d81481a28454b27b171fbc58229a15e1
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-01-23 22:19:29 +0100

    Add cursorline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,10 +10,11 @@ local options = {
   timeout = true,
   timeoutlen = 300,
   sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos",
-  scrolloff = 9999,
+  scrolloff = 10,
   expandtab = true,
   tabstop = 4,
   shiftwidth = 4,
+  cursorline = true,
 
   -- Maximum number of items to show in a popup menu. Useful for limiting
   -- options in autocompletion.
