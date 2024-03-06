d340491674a3fbbcb38b7dc9bec70fdf1896e8c3
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-03-06 15:57:59 +0100

    Add initial neorg config

---
+++ lua/plugins/org.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    event = "VeryLazy",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                work = "~/notes/work",
+                home = "~/notes/home",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+}
