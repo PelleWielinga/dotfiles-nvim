aa0a3d22825a3696b3436101675696a8d807cd62
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-04-17 14:50:04 +0200

    Remove neorg because of lua dependency

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    "nvim-neorg/neorg",
-    event = "VeryLazy",
-
-    -- TODO: This broke. Should I replace it with something?
-    -- build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.keybinds"] = {
-            config = {
-              default_keybinds = true,
-              neorg_leader = "<leader>n",
-            },
-          },
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                work = "~/notes/work",
-                home = "~/notes/home",
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
+-- TODO: This broke. Should I replace it with something?
+--   {
+--     "nvim-neorg/neorg",
+--     event = "VeryLazy",
+--
+--     build = ":Neorg sync-parsers",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function()
+--       require("neorg").setup({
+--         load = {
+--           ["core.defaults"] = {},
+--           ["core.concealer"] = {},
+--           ["core.keybinds"] = {
+--             config = {
+--               default_keybinds = true,
+--               neorg_leader = "<leader>n",
+--             },
+--           },
+--           ["core.dirman"] = {
+--             config = {
+--               workspaces = {
+--                 work = "~/notes/work",
+--                 home = "~/notes/home",
+--               },
+--             },
+--           },
+--         },
+--       })
+--     end,
+--   },
 }
