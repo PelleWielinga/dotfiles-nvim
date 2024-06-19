3ede349ab8c2deada539d046324a8ca903958625
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-06-19 14:03:08 +0200

    neorg fixes

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,32 +1,31 @@
 return {
--- TODO: This broke. Should I replace it with something?
---   {
---     "nvim-neorg/neorg",
---     event = "VeryLazy",
---
---     build = ":Neorg sync-parsers",
---     dependencies = { "nvim-lua/plenary.nvim" },
---     config = function()
---       require("neorg").setup({
---         load = {
---           ["core.defaults"] = {},
---           ["core.concealer"] = {},
---           ["core.keybinds"] = {
---             config = {
---               default_keybinds = true,
---               neorg_leader = "<leader>n",
---             },
---           },
---           ["core.dirman"] = {
---             config = {
---               workspaces = {
---                 work = "~/notes/work",
---                 home = "~/notes/home",
---               },
---             },
---           },
---         },
---       })
---     end,
---   },
+  {
+    "nvim-neorg/neorg",
+    event = "VeryLazy",
+    dependencies = { "vhyrro/luarocks.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.tempus"] = {},
+
+          ["core.keybinds"] = {
+            config = {
+              default_keybinds = true,
+              neorg_leader = "<leader>n",
+            },
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                work = "~/notes/work",
+                home = "~/notes/home",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
