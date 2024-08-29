0d6dd5f6f80b4620e5ff564368f0220aa03f5502
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-08-29 15:48:54 +0200

    Add markdown viewer

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+-- A nicer markdown viewer.
+
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+}
