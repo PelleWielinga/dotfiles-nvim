1e1cf08b80fed042c16e8c1aaf1bf60120b7fc13
Pelle Wielinga <pelle.wielinga@gmail.com>
2024-09-29 10:57:10 +0200

    Improve snippets

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   plugins = {
     cmp = {
@@ -13,12 +14,51 @@
         mapping = {
           "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
           "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = # lua
+            ''
+              cmp.mapping(function()
+                luasnip = require("luasnip")
+                if luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif cmp.visible() then
+                  cmp.confirm({select = true})
+                else
+                  cmp.complete()
+                end
+              end, {'i', 's'})
+            '';
+
+          "<S-Tab>" = # lua
+            ''
+              cmp.mapping(function(fallback)
+                luasnip = require("luasnip")
+                if luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, {'i', 's'})
+            '';
+
           "<C-e>" = "cmp.mapping.abort()";
         };
       };
     };
 
-    luasnip.enable = true;
+    luasnip = {
+      enable = true;
+
+      settings = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+      };
+
+      fromVscode = [
+        {
+          lazyLoad = true;
+          paths = "${pkgs.vimPlugins.friendly-snippets}";
+        }
+      ];
+    };
   };
 }
