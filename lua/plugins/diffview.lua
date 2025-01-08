eb3e20474cf5ba97b184c4a613efa35786bfa07d
Pelle Wielinga <pelle.wielinga@gmail.com>
2025-01-08 23:00:58 +0100

    Add diffview plugin

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      {
+        "<leader>gv",
+        "<cmd>DiffviewOpen<cr>",
+        desc = "Diffview open",
+      },
+      {
+        "<leader>gq",
+        "<cmd>DiffviewClose<cr>",
+        desc = "Diffview close",
+      },
+      {
+        "<leader>gh",
+        "<cmd>DiffviewFileHistory %<cr>",
+        desc = "Diffview history",
+      },
+    }
+  }
+}
