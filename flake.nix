ccef0500d5ae899c9e9fb69967482f483d7e5a22
Pelle Wielinga <pelle.wielinga@gmail.com>
2025-01-08 22:24:31 +0100

    Add nix flake

--- flake.nix
+++ flake.nix
@@ -2,37 +2,41 @@
   description = "Neovim config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-          rocks = pkgs.lua51Packages;
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.stylua
 
-              rocks.luafilesystem
-              rocks.busted
-              rocks.luacheck
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.neovim
+              pkgs.ripgrep
+              pkgs.fzf
+
+              pkgs.nil
             ];
+
+            shellHook = ''
+              export NVIM_RUNTIME_PATH="$HOME/.config/nvim"
+            '';
           };
-        }
-      );
+
+          packages.default = pkgs.neovim;
+
+          apps.default = {
+            type = "app";
+            program = "${pkgs.neovim}/bin/nvim";
+          };
+        };
     };
 }
